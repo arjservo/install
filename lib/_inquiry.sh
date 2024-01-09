a2e448c210266880b50af931bfe13f28b910d427
Angelo Servo <arjservo@gmail.com>
2024-01-09 18:33:16 -0300

    Update _inquiry.sh

--- lib/_inquiry.sh
+++ lib/_inquiry.sh
@@ -203,12 +203,12 @@ inquiry_options() {
   print_banner
   printf "${WHITE} 💻 Bem vindo(a) ao Gerenciador Canal Vem Fazer, selecione abaixo a proxima ação!${GRAY_LIGHT}"
   printf "\n\n"
-  printf "   [0] Instalar Canal Vem Fazer\n"
-  printf "   [1] Atualizar Canal Vem FazerP\n"
-  printf "   [2] Deletar Canal Vem Fazer\n"
-  printf "   [3] Bloquear Canal Vem FazerP\n"
-  printf "   [4] Desbloquear Canal Vem Fazer\n"
-  printf "   [5] Alter. dominio Canal Vem Fazer\n"
+  printf "   [0] Instalar [CriaTicket]\n"
+  printf "   [1] Atualizar [CriaTicket]\n"
+  printf "   [2] Deletar [CriaTicket]\n"
+  printf "   [3] Bloquear [CriaTicket]\n"
+  printf "   [4] Desbloquear [CriaTicket]\n"
+  printf "   [5] Alter. dominio [CriaTicket]\n"
   printf "\n"
   read -p "> " option
 
